f6eef07334d3c374fa9f950dc431e224821f75e0
Gitverde85 <rodrigo.verde@hotmail.com>
2026-04-17 11:08:23 -0300

    Lab - Terraform + Ansible com botão de Destroy

---
+++ git_actions_lab_02/infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatecurso12917"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
